60e89dd8dd128b8c682b5b2d10ff15ac275eaddc
lqk <qkliang@foxmail.com>
2018-08-07 09:21:14 +0800

    commit

---
+++ src/com/java/test/Test.java
@@ -0,0 +1,18 @@
+package com.java.test;
+
+import java.util.Date;
+
+public class Test {
+	public static void main(String[] args) {
+		getDate();
+	}
+	/**
+	 * @since 123
+	 * @author lqk
+	 * 
+	 * */
+	private static void getDate() {
+		Date date = new Date();
+		System.out.println(date);
+	}
+}
